5ed89bda92c02c0af840365945e407d932d1efb4
Neylanio <neylaniogomes@gmail.com>
2020-11-03 11:58:44 -0300

    Verificar existencia de substring

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-// Ambiente para testes jscript em questoes URI
+// Verificar se substring está contida em string maior
 
 var input = require("fs").readFileSync("stdin", "utf8");
 
-var valores = input.split(',');
+var valores = input.split('\n');
+let a = valores[0].split('');
+let b = valores[1];
+
+let count = 0;
+
+for(let i=0; i < a.length; i++) {
+  var sub = '';
+  for(let j=i; j <= i + (b.length - 1); j++) {
+    if(a[i + (b.length - 1)] != undefined) {
+      sub+= a[j];
+    }
+  }
+
+  if(sub === b) {
+    count++;
+  }
+}
+
+console.log(count == 1 ? 'SIM' : 'NÃO')
