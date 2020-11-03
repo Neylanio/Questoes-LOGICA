debd27de30db9e26ad06af39e30698d7aee8f8f2
Neylanio <neylaniogomes@gmail.com>
2020-11-03 11:01:34 -0300

    Printar o N-esimo primo

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 
-var valores = input.split(',');
 
 let primos = [];
 
-for(let i=0; i < valores.length; i++) {
-  if(valores[i] !== 1) {
+// selecionar os primos
+// var valores = input.split(',');
+
+// for(let i=0; i < valores.length; i++) {
+//   if(valores[i] !== 1) {
+//     let count = 0;
+//     for(let j=valores[i]; j > 0; j--) {
+//       let total = valores[i] / j;
+//       if (typeof total == 'number' && total % 1 == 0){
+//         count++;
+//       }
+//     }
+//     if(count == 2) {
+//       primos.push(valores[i]);
+//     }
+//   }
+// }
+
+// console.log(primos)
+
+
+// N-ésimo primo
+for(let i = 1; i < 10**10; i++) {
+  if(i != 1) {
     let count = 0;
-    for(let j=valores[i]; j > 0; j--) {
-      let total = valores[i] / j;
+    for(let j=i; j > 0; j--) {
+      let total = i / j;
       if (typeof total == 'number' && total % 1 == 0){
         count++;
       }
     }
     if(count == 2) {
-      primos.push(valores[i]);
+      primos.push(i);
     }
-  }
+    
+    if(primos.length > input) {
+      i = 10**10;
+    }
+  }  
 }
 
-console.log(primos);
+console.log(primos[input-1]);
