efe0fd660b4560bcc3e0de1bf46fe8f08e65cfe6
Neylanio <neylaniogomes@gmail.com>
2020-11-10 11:48:33 -0300

    Calcular volume da esfera dado um raio

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 
-var pieces = input.split('\n');
+let pi = 3.14159;
 
-var piece1 = pieces[0].split(' ');
-var piece2 = pieces[1].split(' ');
+let total = (4/3) * pi * input**3;
 
-let total = (piece1[1] * piece1[2]) + (piece2[1] * piece2[2])
-
-console.log(`VALOR A PAGAR: R$ ${total.toFixed(2)}`)
+console.log(`VOLUME = ${total.toFixed(3)}`)
