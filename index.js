fa1eabb889a952fd88b5be7fb2045b4094476f30
Neylanio <neylaniogomes@gmail.com>
2020-11-10 10:46:24 -0300

    Add listagem em ordem decrescente

--- index.js
+++ index.js
@@ -4,22 +4,50 @@ var input = require("fs").readFileSync("stdin", "utf8");
 
 var values = input.split(',');
 
+
+// Orderm crescente
+
+// for (let i = 0; i < values.length-1; i++) {
+  
+//   let min = i;
+//   for (let j = i + 1; j < values.length; j++) {
+    
+
+//     if(values[j] < values[min]) {
+//       min = j;
+//     }
+
+//   }  
+
+//   if(i != min) {
+//     let aux = values[i];
+//     values[i] = values[min];
+//     values[min] = aux;
+//   }
+
+// }
+
+
+// ------------------------------------------------------
+
+// Ordem decrescente
+
 for (let i = 0; i < values.length-1; i++) {
   
-  let min = i;
+  let max = i;
   for (let j = i + 1; j < values.length; j++) {
     
 
-    if(values[j] < values[min]) {
-      min = j;
+    if(values[j] > values[max]) {
+      max = j;
     }
 
   }  
 
-  if(i != min) {
+  if(i != max) {
     let aux = values[i];
-    values[i] = values[min];
-    values[min] = aux;
+    values[i] = values[max];
+    values[max] = aux;
   }
 
 }
