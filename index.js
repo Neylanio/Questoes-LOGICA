61e9d7b48455628576d621ffe80cf9191b2b3be5
Neylanio <neylaniogomes@gmail.com>
2020-11-10 12:23:24 -0300

    Calcular areas de algumas formas geometricas

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 
 var input = require("fs").readFileSync("stdin", "utf8");
 
-var values = input.split('\n');
+var values = input.split(' ');
 
-const A = values[0]
-const B = values[1]
-const C = values[2]
+const A = Number(values[0]);
+const B = Number(values[1]);
+const C = values[2];
 
-let total = B * C
+let triangulo = (A * C) / 2;
+let circulo = 3.14159 * (C**2);
+let trapezio = ((A + B) * C)/2;
+let quadrado = B**2;
+let retangulo = A * B;
 
-console.log(`NUMBER = ${A}`)
-console.log(`SALARY = U$ ${total.toFixed(2)}`)
+console.log(`TRIANGULO: ${triangulo.toFixed(3)}`)
+console.log(`CIRCULO: ${circulo.toFixed(3)}`)
+console.log(`TRAPEZIO: ${trapezio.toFixed(3)}`)
+console.log(`QUADRADO: ${quadrado.toFixed(3)}`)
+console.log(`RETANGULO: ${retangulo.toFixed(3)}`)
