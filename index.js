e59e267a3e5500da7d0c91be5c27cd964ffc1547
Neylanio <neylaniogomes@gmail.com>
2020-11-10 11:17:04 -0300

    Media ponderada

--- index.js
+++ index.js
@@ -2,4 +2,12 @@
 
 var input = require("fs").readFileSync("stdin", "utf8");
 
-var valores = input.split(',');
+var values = input.split('\n');
+
+const A = values[0]
+const B = values[1]
+const C = values[2]
+
+let total = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+console.log(`MEDIA = ${total.toFixed(1)}`)
