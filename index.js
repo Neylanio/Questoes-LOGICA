7785fe81fa9a56dbe7ea6c47d3cb3603d6963e13
Neylanio <neylaniogomes@gmail.com>
2020-11-12 11:00:00 -0300

    Descobrir o maior numero num array de tamanho 3

--- index.js
+++ index.js
@@ -6,16 +6,9 @@ var values = input.split(' ');
 
 const A = Number(values[0]);
 const B = Number(values[1]);
-const C = values[2];
+const C = Number(values[2]);
 
-let triangulo = (A * C) / 2;
-let circulo = 3.14159 * (C**2);
-let trapezio = ((A + B) * C)/2;
-let quadrado = B**2;
-let retangulo = A * B;
+let maior = (A + B + Math.abs(A - B)) /2;
+let total = (maior + C + Math.abs(maior - C))/2;
 
-console.log(`TRIANGULO: ${triangulo.toFixed(3)}`)
-console.log(`CIRCULO: ${circulo.toFixed(3)}`)
-console.log(`TRAPEZIO: ${trapezio.toFixed(3)}`)
-console.log(`QUADRADO: ${quadrado.toFixed(3)}`)
-console.log(`RETANGULO: ${retangulo.toFixed(3)}`)
+console.log(`${total} eh o maior`)
