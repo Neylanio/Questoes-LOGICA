300de83938fdc509531994cedbd5521bf4ae9ff8
Neylanio <neylaniogomes@gmail.com>
2020-11-12 11:44:16 -0300

    Calcular gasto de combustivel

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ var input = require("fs").readFileSync("stdin", "utf8");
 
 var values = input.split('\n');
 
-var values1 = values[0].split(' ');
-var values2 = values[1].split(' ');
+var autonomia = 12;
+var tempoGasto = Number(values[0]);
+var velocidadeMedia = Number(values[1])
 
-var x1 = Number(values1[0]);
-var x2 = Number(values2[0]);
+let total = (tempoGasto * velocidadeMedia) / autonomia;
 
-var y1 = Number(values1[1]);
-var y2 = Number(values2[1]);
-
-
-const total = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
-
-console.log(total.toFixed(4))
+console.log(total.toFixed(3))
