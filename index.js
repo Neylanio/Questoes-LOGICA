df6cfc6cdac5bcf6d7452b6328e679dbbe42e2e7
Neylanio <neylaniogomes@gmail.com>
2020-12-03 15:13:53 -0300

    Verificar se frase/palavra e palindromo

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 
-function checkPalindromo(str) {   
+function checkPalindromo(str) {
   str = str.replace(/\s/g, '');
-  for(var i = 0; i < str.length / 2; i++) {
-    if(str != ' '){
-      if (str[i] != str[str.length - i - 1]) {
-        return false;
-      }
+  for (var i = 0; i < str.length / 2; i++) {
+    if (str[i] != str[str.length - i - 1]) {
+      return false;
     }
   }
   return true;
