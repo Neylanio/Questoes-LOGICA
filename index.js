6e34158b914d92423ff19b7b82b90dab9576d41c
Neylanio <neylaniogomes@gmail.com>
2020-12-03 15:13:13 -0300

    Verificar se frase/palavra e palindromo

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-// Ambiente para testes jscript em questoes URI
-
 var input = require("fs").readFileSync("stdin", "utf8");
 
-var valores = input.split(',');
+function checkPalindromo(str) {   
+  str = str.replace(/\s/g, '');
+  for(var i = 0; i < str.length / 2; i++) {
+    if(str != ' '){
+      if (str[i] != str[str.length - i - 1]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+console.log(checkPalindromo(input));
