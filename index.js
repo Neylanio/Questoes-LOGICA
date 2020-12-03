e6bdd574ae0ff324ba8e5bb3c848bb8100ad8525
Neylanio <neylaniogomes@gmail.com>
2020-12-03 15:43:35 -0300

    Retirar qualquer acentuacao ou caracter especial

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 
 function checkPalindromo(str) {
-  str = str.replace(/\s/g, '');
+  str = str.normalize("NFD").replace(/[^a-zA-Zs]/g, "");
   for (var i = 0; i < str.length / 2; i++) {
-    if (str[i].toUpperCase() != str[str.length - i - 1].toUpperCase()) {
+    if (str[i].toUpperCase() !== str[str.length - i - 1].toUpperCase()) {
       return false;
     }
   }
