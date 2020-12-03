3defecde1b5c7b583e07e7e174b2c034fa9b4a50
Neylanio <neylaniogomes@gmail.com>
2020-12-03 15:18:08 -0300

    Verificar entre letras maiusculas e minusculas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var input = require("fs").readFileSync("stdin", "utf8");
 function checkPalindromo(str) {
   str = str.replace(/\s/g, '');
   for (var i = 0; i < str.length / 2; i++) {
-    if (str[i] != str[str.length - i - 1]) {
+    if ((str[i].toUpperCase() != str[str.length - i - 1].toUpperCase()) || (str[i].toLowerCase() != str[str.length - i - 1].toLowerCase())) {
       return false;
     }
   }
