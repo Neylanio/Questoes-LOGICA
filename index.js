1f53e3c5cf1ecdfa57caf82833a72f2a03b893dd
Neylanio <neylaniogomes@gmail.com>
2020-12-04 15:34:42 -0300

    Encontrar numero na posicao solicitada em uma lista encadeada

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-// Ambiente para testes jscript em questoes URI
+// Ambiente para testes jscript
+
+// Encontrar numero na posição solicitada. Posições iniciadas em 1
 
 var input = require("fs").readFileSync("stdin", "utf8");
 
-var valores = input.split(',');
+var entradas = input.split('\n');
+
+var numeros = entradas[0].split(',');
+
+var position = entradas[1];
+
+
+function findPosition(numeros, position) {
+  let count = 1;
+  for(let i=0; i < numeros.length; i++) {
+    if(count == position) {
+      return numeros[i-1];
+    }
+
+    if(i == numeros.length-1) {
+      i = 0;
+    }
+
+    count++;
+  }
+}
+
+console.log(`${numeros}\n`)
+
+console.log(findPosition(numeros, position));
+
