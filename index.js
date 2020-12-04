c9c6452748a82a45053682164fd4f8b0b6e4a02d
Neylanio <neylaniogomes@gmail.com>
2020-12-04 15:15:58 -0300

    Demonstrar o LIFO ou pilha

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-// Ambiente para testes jscript em questoes URI
+// Ambiente para testes jscript
+
+
+// Lógica da PILHA - LIFO
 
 var input = require("fs").readFileSync("stdin", "utf8");
 
 var valores = input.split(',');
+
+console.log(`${valores}\n`)
+
+while(valores.length > 0) {
+  valores.pop();
+  console.log(`${valores}\n`)
+}
